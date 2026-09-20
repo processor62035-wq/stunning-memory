594455f2b8e2bb92a316badea36736960b2c7002
processor62035-wq <processor62035-wq@users.noreply.github.com>
2026-09-20 15:15:12 +0900

    Add configurable measurement refresh modes

--- zigbee-ikea-grillplats-plug/src/init.lua
+++ zigbee-ikea-grillplats-plug/src/init.lua
@@ -8,8 +8,25 @@ local OnOff = clusters.OnOff
 local ElectricalMeasurement = clusters.ElectricalMeasurement
 
 local REPORT_MIN = 1
-local REPORT_MAX = 60
-local POLL_SECONDS = 60
+local REPORT_DISABLED = 0xFFFF
+
+local function get_poll_bounds(device)
+  local mode = device.preferences.pollMode or "variable_5_10"
+  if mode == "variable_5_30" then
+    return 5, 30
+  elseif mode == "fixed_5" then
+    return 5, 5
+  elseif mode == "fixed_10" then
+    return 10, 10
+  elseif mode == "fixed_30" then
+    return 30, 30
+  elseif mode == "fixed_60" then
+    return 60, 60
+  elseif mode == "manual" then
+    return nil, nil
+  end
+  return 5, 10
+end
 
 local function read_measurements(device)
   device:send(OnOff.attributes.OnOff:read(device))
@@ -49,12 +66,14 @@ local function save_multiplier(field, default)
 end
 
 local function configure_reporting(driver, device)
+  local _, report_max = get_poll_bounds(device)
+  report_max = report_max or REPORT_DISABLED
   device:send(device_management.build_bind_request(device, OnOff.ID, driver.environment_info.hub_zigbee_eui))
   device:send(device_management.build_bind_request(device, ElectricalMeasurement.ID, driver.environment_info.hub_zigbee_eui))
-  device:send(OnOff.attributes.OnOff:configure_reporting(device, 0, REPORT_MAX))
-  device:send(ElectricalMeasurement.attributes.RMSVoltage:configure_reporting(device, REPORT_MIN, REPORT_MAX, 1))
-  device:send(ElectricalMeasurement.attributes.RMSCurrent:configure_reporting(device, REPORT_MIN, REPORT_MAX, 1))
-  device:send(ElectricalMeasurement.attributes.ActivePower:configure_reporting(device, REPORT_MIN, REPORT_MAX, 1))
+  device:send(OnOff.attributes.OnOff:configure_reporting(device, 0, 300))
+  device:send(ElectricalMeasurement.attributes.RMSVoltage:configure_reporting(device, REPORT_MIN, report_max, 1))
+  device:send(ElectricalMeasurement.attributes.RMSCurrent:configure_reporting(device, REPORT_MIN, report_max, 1))
+  device:send(ElectricalMeasurement.attributes.ActivePower:configure_reporting(device, REPORT_MIN, report_max, 1))
   device:send(ElectricalMeasurement.attributes.ACVoltageMultiplier:read(device))
   device:send(ElectricalMeasurement.attributes.ACVoltageDivisor:read(device))
   device:send(ElectricalMeasurement.attributes.ACCurrentMultiplier:read(device))
@@ -67,8 +86,13 @@ end
 local function schedule_polling(driver, device)
   local timer = device:get_field("measurement_poll_timer")
   if timer ~= nil then device.thread:cancel_timer(timer) end
-  timer = device.thread:call_on_schedule(POLL_SECONDS, function()
+  device:set_field("measurement_poll_timer", nil)
+  local min_seconds, max_seconds = get_poll_bounds(device)
+  if min_seconds == nil then return end
+  local delay = math.random(min_seconds, max_seconds)
+  timer = device.thread:call_with_delay(delay, function()
     read_measurements(device)
+    schedule_polling(driver, device)
   end, "grillplats_measurement_poll")
   device:set_field("measurement_poll_timer", timer)
 end
@@ -89,12 +113,17 @@ local driver_template = {
       end, "grillplats_configure")
     end,
     init = function(driver, device)
+      math.randomseed(os.time())
       device:set_field("voltage_multiplier", device:get_field("voltage_multiplier") or 1, {persist = true})
       device:set_field("voltage_divisor", device:get_field("voltage_divisor") or 10, {persist = true})
       device:set_field("current_multiplier", device:get_field("current_multiplier") or 1, {persist = true})
       device:set_field("current_divisor", device:get_field("current_divisor") or 1000, {persist = true})
       device:set_field("power_multiplier", device:get_field("power_multiplier") or 1, {persist = true})
       device:set_field("power_divisor", device:get_field("power_divisor") or 1, {persist = true})
+      schedule_polling(driver, device)
+    end,
+    infoChanged = function(driver, device)
+      configure_reporting(driver, device)
       schedule_polling(driver, device)
     end,
     doConfigure = configure_reporting,
